e9e2ff7b04d3dcfbe7e8447e46440827a96abbae
Radek Vaculik <vaculik@organizetube.com>
2017-03-29 23:21:55 +0200

    tweeting the first message of this bot, ever

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ let Twitter = require('twitter');
 let myVars = require('./myVars.js');
 
 console.log(myVars)
+
+let client = new Twitter({
+  consumer_key : myVars.consumer_key ,
+  consumer_secret : myVars.consumer_secret ,
+  access_token_key : myVars.access_token_key ,
+  access_token_secret : myVars.access_token_secret
+})
+
+let params = {screen_name: 'nodejs'}
+
+let testStatus = " First tweet for this one. Machine Learning..."
+
+client.post('statuses/update', { status : testStatus}, function(error, tweet, response) {
+  if(error) {
+    console.log(error)
+    console.log(tweet)
+    console.log(response)
+  } else {
+    console.log("tweeted fine?")
+  }
+})
